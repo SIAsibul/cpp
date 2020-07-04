a5a7cb45af6175155df2457aaa9ed084f85d78be
Asibul Islam <48784984+SIAsibul@users.noreply.github.com>
2020-07-04 12:34:06 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+
+/*-----------------------------------------------------------------------------------/*
+      _     ____  ___  ____   _   _  _         ___  ____   _         _     __  __
+     / \   / ___||_ _|| __ ) | | | || |       |_ _|/ ___| | |       / \   |  \/  |
+    / _ \  \___ \ | | |  _ \ | | | || |        | | \___ \ | |      / _ \  | |\/| |
+   / ___ \  ___) || | | |_) || |_| || |___     | |  ___) || |___  / ___ \ | |  | |
+  /_/   \_\|____/|___||____/  \___/ |_____|   |___||____/ |_____|/_/   \_\|_|  |_|
+
+   _   __           __ __ __
+  |_) (_  _  o__   /  (_ |_
+  |_)o__)(_  || |  \____)|__
+
+  ╔═╗┬─┐┌─┐┌─┐┌┐┌ ╦ ╦┌┐┌┬┬  ┬┌─┐┬─┐┌─┐┬┌┬┐┬ ┬ ┌─┐┌─┐ ╔╗ ┌─┐┌┐┌┌─┐┬  ┌─┐┌┬┐┌─┐┌─┐┬ ┬
+  ║ ╦├┬┘├┤ ├┤ │││ ║ ║││││└┐┌┘├┤ ├┬┘└─┐│ │ └┬┘ │ │├┤  ╠╩╗├─┤││││ ┬│  ├─┤ ││├┤ └─┐├─┤
+  ╚═╝┴└─└─┘└─┘┘└┘ ╚═╝┘└┘┴ └┘ └─┘┴└─└─┘┴ ┴  ┴  └─┘└   ╚═╝┴ ┴┘└┘└─┘┴─┘┴ ┴─┴┘└─┘└─┘┴ ┴
+
+  FB: https://facebook.com/SIAsibul
+
+/*----------------------------------------------------------------------------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, int> li;
+typedef pair<int, ll> il;
+typedef vector<ii> vii;
+typedef vector<il> vil;
+typedef vector<li> vli;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define sz size()
+#define all(a)  a.begin(), a.end()
+#define mem(a, b)     memset(a, b, sizeof(a))
+#define f0(i,b) for(int i=0;i<(b);i++)
+#define f1(i,b) for(int i=1;i<=(b);i++)
+#define f2(i,a,b) for(int i=(a);i<=(b);i++)
+#define fr(i,b,a) for(int i=(b);i>=(a);i--)
+#define rep(i,a,b,c) for(int i=(a);i!=(b);i+=(c))
+#define nl "\n"
+
+
+const int INF = 1e9 + 5;
+const int MXN = 2e5 + 5;
+
+
+
+int main(){
+
+    return 0;
+}
